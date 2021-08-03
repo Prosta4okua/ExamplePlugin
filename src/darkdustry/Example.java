3f0a2f3e175ac1b1de72457ad89737b56c65b71e
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-08-03 13:27:25 +0300

    Первоначальные экзамплы

---
+++ src/darkdustry/Example.java
@@ -0,0 +1,29 @@
+//Пакет, в котором лежит код
+package darkdustry;
+
+//Импорты пакетов из arc
+import arc.util.*;
+
+//Импорты пакетов из mindustry
+import mindustry.mod.*;
+
+public class Example extends Plugin {
+
+    //Метод init() выполняется, когда сервер запускается
+    @Override
+    public void init() {
+
+    }
+
+    //Этот метод содержит все слеш-команды для игроков
+    @Override
+    public void registerClientCommands(CommandHandler handler) {
+
+    }
+
+    //Этот метод содержит все команды для консоли
+    @Override
+    public void registerServerCommands(CommandHandler handler) {
+
+    }
+}
